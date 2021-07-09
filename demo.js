24e2cd72aa8d24e6829e8ab1a034e58e3c190091
Jacky Chen <jacky.chen0319@gmail.com>
2021-07-08 18:52:31 -0700

    Finalized for presentation

--- demo.js
+++ demo.js
@@ -1,33 +1,34 @@
 // const basicAnimation = anime({
-//   targets: '.basic',
+//   targets: '.demo',
 //   translateX: 250,
+//   easing: 'easeInOutSine',
 // })
 
-const multipleAnimation = anime({
-  targets: '.js-box',
-  translateX: (elm, index, t) => index * 50,
-  scale: 2,
-  easing: 'easeInOutSine',
-  delay: (elm, index, t) => index * 20,
-  duration: 1200,
-  loop: true,
-  direction: 'alternate',
-})
-
-// const boxesAnimation = anime({
-//   targets: '.js-box',
-//   translateY: [150, 50],
-//   backgroundColor: (el, i, t) => {
-//     const r = 58 + (i * 12);
-//     const g = 35 + (i * 12);
-//     const b = 220;
-//     const color = `rgb(${r}, ${g}, ${b})`;
-//     return color;
-//   },
-//   duration: 900,
-//   easing: 'easeOutElastic',
-//   elasticity: 500,
-//   delay: (el, i, t) => i * 20,
+// const multipleAnimation = anime({
+//   targets: '.demo',
+//   translateX: (elm, index, t) => index * 50,
+//   scale: 2,
+//   easing: 'easeInOutSine',
+//   delay: (elm, index, t) => index * 20,
+//   duration: 1200,
 //   loop: true,
 //   direction: 'alternate',
-// });
+// })
+
+const boxesAnimation = anime({
+  targets: '.js-box',
+  translateY: [150, 50],
+  backgroundColor: (el, i, t) => {
+    const r = 58 + (i * 12);
+    const g = 35 + (i * 12);
+    const b = 220;
+    const color = `rgb(${r}, ${g}, ${b})`;
+    return color;
+  },
+  duration: 900,
+  easing: 'easeOutElastic',
+  elasticity: 500,
+  delay: (el, i, t) => i * 20,
+  loop: true,
+  direction: 'alternate',
+});
